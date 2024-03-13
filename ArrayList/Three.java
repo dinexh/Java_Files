f7a3c1cb161d35c253b5a7f817d4b6ba8420d03e
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-03-13 23:04:42 +0530

    Files Updated

--- ArrayList/Three.java
+++ ArrayList/Three.java
@@ -1,11 +1,12 @@
-import java.util.ArrayList;
+import java.util.*;
+// import java.util.ArrayList;
 public class Three {
         public static void main(String[] args) {
             ArrayList<String> CityName = new ArrayList<>(2);
             CityName.add("Vijayawada");
             CityName.add("Hyderabad");
             CityName.add("Vizag");
-            // System.out.println(CityName);
+            // System.out.println(CityName);aa
             CityName.set(1,"Chennai");
             // System.out.println(CityName);
             CityName.remove(0);
