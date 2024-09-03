917daebdaa67d1668762d763f51e062707d85423
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-09-03 12:57:36 +0530

    added new files

---
+++ Exam/Comparable.java
@@ -0,0 +1,8 @@
+public class Student implements comparable<student>
+{
+int rollno;
+String name;
+int age;
+Student(int rollno,String name,int age)
+{
+
---
+++ OddSem/ThirdSem/src/Generics.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Generics {
+    public static void main(String[] args) {
+    ArrayList arrayList = new ArrayList();
+    arrayList.add("Hello");
+    arrayList.add(10);
+    arrayList.add(60);
+    int a = arrayList.get(1);
+    }
+}
